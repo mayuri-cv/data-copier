d146f385aee24e02dad1f94950f0be4cf099c312
mayurid <mayuri.dangare@clairvoyantsoft.com>
2022-04-26 13:38:12 +0530

    Dockerized the application

---
+++ dockercommand.sh
@@ -0,0 +1,1 @@
+docker run -it --name data-copier --rm -v "C:\Users\Mayuri Dangare\Research\data\retail_db_json:/retail_db_json" -e BASE_DIR=/retail_db_json -e DB_HOST=65468e58a377 -e DB_PORT=5432 -e DB_NAME=retail_db -e DB_USER=retail_user -e DB_PASS=itversity data-copier bash
